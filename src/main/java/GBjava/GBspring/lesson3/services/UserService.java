30d4eeb87126a17152e02fdf0091abeb85243789
loxlouro <loxlouro2014@gmail.com>
2024-01-24 16:41:56 +0300

    UserService.java methods

--- src/main/java/GBjava/GBspring/lesson3/services/UserService.java
+++ src/main/java/GBjava/GBspring/lesson3/services/UserService.java
@@ -1,13 +1,52 @@
 package GBjava.GBspring.lesson3.services;
 
+import GBjava.GBspring.lesson3.models.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import GBjava.GBspring.lesson3.repo.UserRepository;
 
+import java.util.Collection;
+
 @Service
 public class UserService {
     @Autowired
     UserRepository userRepository;
-
+    public String saveUser(User user){
+        try{
+            User userFromDB = userRepository.findByEmail(user.getEmail());
+            if (userFromDB!=null) throw new RuntimeException("User already exists");
+            userRepository.save(user);
+            return "User saved successfully";
+        } catch (RuntimeException e){
+            return e.getMessage();
+        }
+    }
+    public String deleteUser(User user){
+        try{
+            User userFromDB = userRepository.findByEmail(user.getEmail());
+            if (userFromDB==null) throw new RuntimeException("User not found");
+            userRepository.delete(userFromDB);
+            return "User deleted successfully";
+        } catch (RuntimeException e){
+            return e.getMessage();
+        }
+    }
+    public User findByEmail(String email){
+        try{
+            User userFromDB = userRepository.findByEmail(email);
+            if (userFromDB==null) throw new RuntimeException("User not found");
+            return userFromDB;
+        }catch (RuntimeException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+    public Collection<User> getAllUsers(){
+        try{
+            Collection<User> users = userRepository.findAll();
+            return users;
+        } catch (Exception e){
+            return null;
+        }
+    }
 }
