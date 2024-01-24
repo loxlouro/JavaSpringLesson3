28d7d73572e379b1f0778195493f1f5d94f220a5
loxlouro <loxlouro2014@gmail.com>
2024-01-24 17:22:01 +0300

    UserController.java

--- src/main/java/GBjava/GBspring/lesson3/controllers/UserController.java
+++ src/main/java/GBjava/GBspring/lesson3/controllers/UserController.java
@@ -1,15 +1,34 @@
 package GBjava.GBspring.lesson3.controllers;
 
+import GBjava.GBspring.lesson3.models.User;
 import GBjava.GBspring.lesson3.services.UserService;
 import jakarta.persistence.Access;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @CrossOrigin
 public class UserController {
     @Autowired
     UserService userService;
+    @RequestMapping(path = "/getAllUsers",method = RequestMethod.GET)
+    public Collection<User> getAllUsers(){
+        return userService.getAllUsers();
+    }
 
+    @RequestMapping(path = "/saveUser", method = RequestMethod.POST)
+    public String saveUser(@RequestParam("user") User user){
+        return userService.saveUser(user);
+    }
+
+    @RequestMapping(path = "/deleteUser", method = RequestMethod.DELETE)
+    public String deleteUser(@RequestParam("user") User user){
+        return userService.deleteUser(user);
+    }
+    @RequestMapping(path = "/getUser/{email}", method = RequestMethod.GET)
+    public User getUserByEmail(@PathVariable("email") String email){
+        return userService.findByEmail(email);
+    }
 }
--- src/main/java/GBjava/GBspring/lesson3/repo/UserRepository.java
+++ src/main/java/GBjava/GBspring/lesson3/repo/UserRepository.java
@@ -1,9 +1,8 @@
 package GBjava.GBspring.lesson3.repo;
 
 import GBjava.GBspring.lesson3.models.User;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> , ElasticsearchRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByEmail(String email);
 }
